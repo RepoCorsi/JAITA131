a34a89fba52f12b06c3707b94b8f7b2b15fdd14d
RepoCorsi <ponghissimo@yahoo.it>
2024-11-04 20:51:28 +0100

    Add files via upload

---
+++ OOP/Frigorifero/Cibo.java
@@ -0,0 +1,25 @@
+package rigorifero;
+
+public class Cibo {
+	String nome;
+	
+	public Cibo(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
+	
+	
+	
+}
--- OOP/Frigorifero/Frigorifero.java
+++ OOP/Frigorifero/Frigorifero.java
@@ -1,1 +1,45 @@
+package rigorifero;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Frigorifero {
+
+	public static void main(String[] args) {
+
+		Cibo[] arrCibi= new Cibo[3];//Array di cibi
+//		
+//		Cibo cibo1= new Cibo("pizza");
+//		Cibo cibo2= new Cibo("hamburger");
+//		Cibo cibo3= new Cibo("hot dog");
+//		
+//		arrCibi[0]=cibo1;
+//		arrCibi[1]=cibo2;
+//		arrCibi[2]=cibo3;
+//		
+//		System.out.println("nel frigorifero ci sono i seguenti cibi:");
+//		for (Cibo cibo : arrCibi) {
+//			System.out.println(cibo);//toString
+//		}
+
+		//oggetti anonimi		
+		Cibo[] dispensa= {new Cibo("pizza"),new Cibo("hamburger"),new Cibo("hot dog")};
+		for (Cibo cibo : dispensa) {
+			System.out.println(cibo);//toString
+		}
+		
+		//trasformato l'array in un ArrayList
+		ArrayList<Cibo> lista= new ArrayList<>(Arrays.asList(dispensa));
+		lista.add(new Cibo("pasta"));//ho aggiunto un nuovo oggetto alla lista
+		
+		System.out.println("nel frigorifero ci sono i seguenti cibi:");
+		lista.forEach(cibo->System.out.println(cibo));
+		
+		
+		
+		
+		
+	}
+
+}
