3817bf369ae2978d5707130851ed68b5f792ef8e
RepoCorsi <ponghissimo@yahoo.it>
2026-09-12 23:35:52 +0200

    Add files via upload

--- OOP/Interfacce/testUsabile/Usabile.java
+++ OOP/Interfacce/testUsabile/Usabile.java
@@ -1,12 +1,12 @@
 package testUsabile;
 
 //Gestisce i comportamenti,
-//non gerarchico 
+//non gerarchico
 //tutti i metodi dentro l'interfaccia sono public abstract
-//se ho degli attributi sono per default public stati final
+//se ho degli attributi sono per default public static final
 //si comportano come le classi-> eredito i metodi pubblici
 public interface Usabile {
 
 	void usa();//di default public e abstract
-	
+
 }
